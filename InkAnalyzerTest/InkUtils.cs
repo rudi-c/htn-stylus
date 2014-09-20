55753f99a288bafa71a8609384352826593793d4
Geoffry Song <goffrie@gmail.com>
2014-09-20 12:12:07 -0400

    Add InkUtils.toPolyline.
    Contains completely bullshitted heuristics.

--- InkAnalyzerTest/InkUtils.cs
+++ InkAnalyzerTest/InkUtils.cs
@@ -12,7 +12,7 @@ namespace InkAnalyzerTest
 {
     class InkUtils
     {
-        public static StrokeCollection Scale(StrokeCollection strokes, double scale)
+        public static void Scale(StrokeCollection strokes, double scale)
         {
             foreach (Stroke stroke in strokes)
                 for (int i = 0; i < stroke.StylusPoints.Count; i++)
@@ -20,6 +20,54 @@ namespace InkAnalyzerTest
                     stroke.StylusPoints[i] = new StylusPoint(
                         stroke.StylusPoints[i].X * scale,
                         stroke.StylusPoints[i].Y * scale);
+        }
+
+        public static double distSquared(StylusPoint p1, StylusPoint p2)
+        {
+            double dx = p1.X - p2.X, dy = p1.Y - p2.Y;
+            return dx * dx + dy * dy;
+        }
+
+        public static double lineDistSquared(StylusPoint a, StylusPoint b, StylusPoint p)
+        {
+            double bx = b.X - a.X, by = b.Y - a.Y;
+            double px = p.X - a.X, py = p.Y - a.Y;
+            double dot = bx * px + by * py;
+            double projScale = dot / (bx * bx + by * by);
+            if (projScale < 0) projScale = 0;
+            else if (projScale > 1) projScale = 1;
+            double perpX = px - bx * projScale, perpY = py - by * projScale;
+            return perpX * perpX + perpY * perpY;
+        }
+
+        public static StylusPointCollection toPolyline(StylusPointCollection points)
+        {
+            StylusPointCollection r = new StylusPointCollection();
+            if (points.Count == 0) return r;
+            r.Add(points[0]);
+            for (int i = 0; i+1 < points.Count;)
+            {
+                int j = i+2;
+                StylusPoint p1 = points[i];
+                while (j < points.Count)
+                {
+                    StylusPoint p2 = points[j];
+                    double maxDeviationSquared = 20 * (Math.Log(distSquared(p1, p2) + 1)+1);
+                    for (int k = i + 1; k < j; ++k)
+                    {
+                        if (lineDistSquared(p1, p2, points[k]) > maxDeviationSquared)
+                        {
+                            goto stop;
+                        }
+                    }
+                    ++j;
+                }
+            stop:
+                --j;
+                r.Add(points[j]);
+                i = j;
+            }
+            return r;
         }
     }
 }
